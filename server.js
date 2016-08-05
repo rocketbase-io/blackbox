8fb7b2f52ca8589964e06a4aecaa00101e80a183
Niels Schelbach <niels.schelbach@rocketbase.io>
2016-08-05 22:19:25 +0200

    -working on server now

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 require('./lib/bus/event');
-require('./lib/model/export');
+require('./lib/model/exportsensordata');
 
 
-const sensorinput = require('./lib/input/sensorinputdgram');
+const sensorinput = require('./lib/input/sensorinputtcp');
 const clientoutput = require('./lib/output/clientoutput');
 const nmea = require('./lib/processing/nmea');
 const logger = require('./lib/util/logging')('server');
@@ -12,13 +12,13 @@ const logger = require('./lib/util/logging')('server');
 logger.info('starting up server');
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://docker:32768/blackbox');
+mongoose.connect('mongodb://localhost:27017/blackbox');
 
 // first define input
-sensorinput.socket(10110);
+sensorinput.socket('172.16.1.2', 10110);
 
 // at last the output will been sending to client
 clientoutput.socket(5000);
 
 
-nmea.nmeaIncomingProcessor();
+nmea.processNMEA();
